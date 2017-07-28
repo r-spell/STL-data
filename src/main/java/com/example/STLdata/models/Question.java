d6b7912f344e20f7b658f3a618e68db3c123cc55
Rosanna Speller <rosanna.speller@gmail.com>
2017-07-28 11:36:41 -0500

    Clean up question.java to only base of model.

---
+++ src/main/java/com/example/STLdata/models/Question.java
@@ -0,0 +1,17 @@
+package com.example.STLdata.models;
+
+public abstract class Question {
+
+    private String questionText;
+
+    public Question(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+}
+
+
